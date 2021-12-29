770a3b544384cf44d63c0725107a47f9cf41beff
Artem <a.yasenko@gmail.com>
2021-12-29 17:22:43 +0200

    Header corrected

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,31 +10,25 @@ class Header extends Component {
     return (
       <header className="header">
         <div className="content">
-          <a href="/" className="logo">
-            <img src={logo} className="logo__img" alt="Productivity tracker" />
+          <a href="/">
+            <img src={logo} className="logo-img" alt="Productivity tracker" />
           </a>
           <ul className="menu">
-            <li className="menu__item menu__item--add">
-              <a href="" className="icon-add menu__link open-dialog add-task-btn" />
-            </li>
-            <li className="menu__item menu__item--remove">
-              <a href="" id="remove-mode-btn" className="icon-trash menu__link" />
-            </li>
             <li className="menu__item">
-              <NavLink activeStyle={activeStyle} exact to={"/"} className="icon-list menu__link" />
+              <NavLink activeStyle={activeStyle} exact to={"/"} className="menu__link icon-list" />
             </li>
             <li className="menu__item">
               <NavLink
                 activeStyle={activeStyle}
                 to={"/report"}
-                className="icon-statistics menu__link"
+                className="menu__link icon-statistics"
               />
             </li>
             <li className="menu__item">
               <NavLink
                 activeStyle={activeStyle}
                 to={"/settings"}
-                className="icon-settings menu__link"
+                className="menu__link icon-settings"
               />
             </li>
           </ul>
